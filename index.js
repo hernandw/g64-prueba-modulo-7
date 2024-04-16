ce153286c5718ee50aadaceb518cbdea9a3a472f
Williams Hernandez <hernandw@gmail.com>
2024-04-16 15:50:17 -0400

    :tada: resolución de prueba modulo

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import routes from "./routes/routes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//middlewares
 
-
+app.use(express.json());
 //routes
 app.use('/', routes);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,21 @@
 import express from "express";
+
+import { home, addUser, getUser, editUser, deleteUser, addTranfer, getTranfer } from "../controller/userController.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hola mundo desde router");
-})
+router.get("/", home)
+
+router.post("/usuario", addUser)
+
+router.get("/usuarios", getUser)
+
+router.put('/usuario', editUser)
+
+router.delete('/usuario', deleteUser)
+
+router.post('/transferencia', addTranfer)
+
+router.get('/transferencias', getTranfer)
 
 export default router
